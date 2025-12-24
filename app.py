60121f7c7b552849c38685969c028f569ebe995d
huyutao857 <huyutao851029@qq.com>
2025-12-24 11:54:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -760,7 +760,8 @@ def eval_report():
 
 # ========== 启动服务 ==========
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 
 
